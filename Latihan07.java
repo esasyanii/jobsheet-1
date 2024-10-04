b76abbea2a833264124635014f50f40003c4ae3f
esasyanii <syaniday@gmail.com>
2024-10-04 10:24:30 +0700

    Add files via upload

---
+++ Latihan07.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan07
+ */
+public class Latihan07 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Daysyani Sophi Masayu");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ TugasJava07.java
@@ -0,0 +1,18 @@
+/**
+ * TugasJava07
+ */
+public class TugasJava07 {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("-----Data Mahasiswa Jurusan Teknologi Informasi POLINEMA-----");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Nama      : Daysyani Sophi Masayu");
+        System.out.println("NIM       : 244107060153");
+        System.out.println("No Absen  : 07");
+        System.out.println(" Prodi    : D IV Sistem Informasi Bisnis");
+        System.out.println("Alamat    : Trenggalek");
+        System.out.println("No HP     : 08563737292");
+        System.out.println("Hobi      : Membaca");
+    }
+}
